a4b535540720eb9246db7a772ec03d4a438db4a6
sskorna <s.skorna@gmail.com>
2018-11-12 11:28:38 +0100

    modified app

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
-import os
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 data = pd.read_csv("MatchInfo.csv")
+data_cum = pd.read_csv("MatchInfoCum.csv")
+data_cum = data_cum[6:201]
 team_averages = [1000, 1000, 1000]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -19,45 +21,94 @@ app.layout = html.Div(children=[
     html.H1(children='Spectators in Tipsport league'),
 
     html.Div(children='''
-        Select team
+        Select hockey club
     '''),
-             
-    dcc.Dropdown(
-        id='my-dropdown',
-        options=[
-            {'label': 'Trenčín', 'value': 'TN'},
-            {'label': 'Banská Bystrica', 'value': 'BB'},
-            {'label': 'Košice', 'value': 'KE'},
-            {'label': 'Nitra', 'value': 'NR'},
-            {'label': 'Žilina', 'value': 'ZI'},
-            {'label': 'Zvolen', 'value': 'ZV'},
-            {'label': 'Poprad', 'value': 'PP'},
-            {'label': 'Nové Zámky', 'value': 'NZ'},
-            {'label': 'Liptovský Mikuláš', 'value': 'LM'}
-        ],
-        value='BB'
-    ),
-    
-    dcc.Graph(
-        id='my-graph',
-        figure={
-            'data': [{
-                    'x': ["2016/2017", "2017/2018", "2018/2019"],
-                    'y': team_averages,
-                    'type': 'bar',
-                    'name': u'Montréal'
-            }],
-            'layout': {
-                'title': 'Average attendance'
-            }
-        }
-    )
+    html.Div([
+        html.Div([
+            dcc.Dropdown(
+                id='my-dropdown',
+                options=[
+                    {'label': 'Trenčín', 'value': 'TN'},
+                    {'label': 'Banská Bystrica', 'value': 'BB'},
+                    {'label': 'Košice', 'value': 'KE'},
+                    {'label': 'Nitra', 'value': 'NR'},
+                    {'label': 'Žilina', 'value': 'ZI'},
+                    {'label': 'Zvolen', 'value': 'ZV'},
+                    {'label': 'Poprad', 'value': 'PP'},
+                    {'label': 'Nové Zámky', 'value': 'NZ'},
+                    {'label': 'Liptovský Mikuláš', 'value': 'LM'},
+                    {'label': 'Detva', 'value': 'DT'},
+                    {'label': 'Martin', 'value': 'MT'},
+                    {'label': 'Budapest', 'value': 'BU'},
+                    {'label': 'Miskolc', 'value': 'MI'}
+#                    ,
+#                    {'label': 'all', 'value': 'all'}
+                ],
+                value='BB'
+            ),
+            
+            dcc.Graph(
+                id='my-graph',
+                figure={
+                    'data': [{
+                            'x': ["2016/2017", "2017/2018", "2018/2019"],
+                            'y': team_averages,
+                            'type': 'bar',
+                            'name': u'Montréal'
+                    }],
+                    'layout': {
+                        'title': 'Average attendance per club'
+                    }
+                }
+            )
+        ], className="six columns"),
+        html.Div([
+            dcc.Graph(
+                id='g2',
+                figure=go.Figure(
+                    data=[
+                        go.Scatter(
+                            x = np.arange(1,196),
+                            y = data_cum["0"],
+                            name = "2016/2017",
+                            line = dict(
+                                color = ('rgb(205, 12, 24)'),
+                                width = 4
+                            )
+                        ),
+                        go.Scatter(
+                            x = np.arange(1,196),
+                            y = data_cum["1"],
+                            name = "2017/2018",
+                            line = dict(
+                                color = ('rgb(55, 83, 109)'),
+                                width = 4
+                            )
+                        ),
+                        go.Scatter(
+                            x = np.arange(1,196),
+                            y = data_cum["2"],
+                            name = "2018/2019",
+                            line = dict(
+                                color = ('rgb(26, 118, 255)'),
+                                width = 4
+                            )
+                        )
+                    ],
+                layout = dict(title = 'Cumulative sum of spectators over time')
+                )
+            )
+        ], className="six columns"),
+    ], className="row")
 ])
     
 @app.callback(Output('my-graph', 'figure'), [Input('my-dropdown', 'value')])
 def update_graph(selected_dropdown_value):
     
-    plotdata = data.loc[data.loc[:,"sHome"] == selected_dropdown_value
+    if selected_dropdown_value == "all":
+        plotdata = data[{"iSpectator", "dDate"}]
+    else:
+        plotdata = data.loc[data.loc[:,"sHome"] == selected_dropdown_value
                     , {"iSpectators","dDate"}]
     team_averages = []
     for j in range (1, 4):
@@ -75,7 +126,7 @@ def update_graph(selected_dropdown_value):
             'name': u'Montréal'
         }],
         'layout': {
-            'title': 'Average attendance'
+            'title': 'Average attendance by club'
         }
     }
 
